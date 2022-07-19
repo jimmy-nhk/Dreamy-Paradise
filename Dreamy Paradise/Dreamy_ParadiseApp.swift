48ed4da701905187b38ad2cd41a7f5fe7ebb6ed8
jimmy-nhk <56537411+jimmy-nhk@users.noreply.github.com>
2022-07-19 16:27:09 +0700

    Initial Commit

---
+++ Dreamy Paradise/Dreamy_ParadiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dreamy_ParadiseApp.swift
+//  Dreamy Paradise
+//
+//  Created by Jimmy on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dreamy_ParadiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
